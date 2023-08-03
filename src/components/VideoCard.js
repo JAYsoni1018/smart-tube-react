17ec13bf81edf10ae3d2f8da891393af4d9806ed
jay <jaysoni1385@gmail.com>
2023-08-03 22:11:51 +0530

    final

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -18,7 +18,7 @@ const VideoCard = ({ video }) => {
     return (
 
         video?.title ? <>
-            <Card sx={{ width: { md: "261px", sm: "377px", xs: "290px", background: "#1e1e1e" } }} >
+            <Card sx={{ width: { md: "301px", sm: "377px", xs: "290px", background: "#1e1e1e" } }} >
                 <Link to={video?.videoId ? `/video/${video?.videoId}` : ""}>
             {/* {video.videoId ? `/video/${video.videoId}` : demoVideoUrl}  */}
                     <img src={video?.thumbnails[0].url} style={{ height: "209px", width: { md: "261px", sm: "100%", xs: "200px" }, backgroundSize: "auto" }} alt={video?.title} />
